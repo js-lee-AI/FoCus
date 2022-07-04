ce06ca6d4be8e6f4d816f9153389ccbdb8b8d9aa
JungSeob Lee <60927808+js-lee-AI@users.noreply.github.com>
2022-07-04 18:54:07 +0900

    Update tfidf.py

--- python_tf_idf/tfidf.py
+++ python_tf_idf/tfidf.py
@@ -1,9 +1,16 @@
 from rank_bm25 import BM25Okapi
 from sentence_transformers import SentenceTransformer, util
 
-DPR_PASSAGE_ENCODER = SentenceTransformer('facebook-dpr-ctx_encoder-single-nq-base', device='cuda')
-DPR_QUERY_ENCODER = SentenceTransformer('facebook-dpr-question_encoder-single-nq-base', device='cuda')
-STS_MODEL = SentenceTransformer('sentence-transformers/multi-qa-MiniLM-L6-dot-v1', device='cuda')
+DPR_PASSAGE_ENCODER = SentenceTransformer(
+    "facebook-dpr-ctx_encoder-single-nq-base", device="cuda"
+)
+DPR_QUERY_ENCODER = SentenceTransformer(
+    "facebook-dpr-question_encoder-single-nq-base", device="cuda"
+)
+STS_MODEL = SentenceTransformer(
+    "sentence-transformers/multi-qa-MiniLM-L6-dot-v1", device="cuda"
+)
+
 
 class TfIdf:
     def __init__(self):
@@ -15,7 +22,7 @@ class TfIdf:
         # building a dictionary
         doc_dict = {}
         for w in list_of_words:
-            doc_dict[w] = doc_dict.get(w, 0.) + 1.0
+            doc_dict[w] = doc_dict.get(w, 0.0) + 1.0
             self.corpus_dict[w] = self.corpus_dict.get(w, 0.0) + 1.0
 
         # normalizing the dictionary
@@ -28,7 +35,7 @@ class TfIdf:
 
     def similarities(self, list_of_words):
         """Returns a list of all the [docname, similarity_score] pairs relative to a
-list of words.
+        list of words.
 
         """
 
@@ -49,7 +56,9 @@ list of words.
             doc_dict = doc[1]
             for k in query_dict:
                 if k in doc_dict:
-                    score += (query_dict[k] / self.corpus_dict[k]) + (doc_dict[k] / self.corpus_dict[k])
+                    score += (query_dict[k] / self.corpus_dict[k]) + (
+                        doc_dict[k] / self.corpus_dict[k]
+                    )
             sims.append([doc[0], score])
 
         return sims
@@ -76,7 +85,6 @@ class BM25:
         for idx, qt in enumerate(doc_scores):
             self.idx_and_similarity_list.append([idx, qt])
 
-
         # print(f"knowledge_candidate: {self.knowledge_candidate}")
         # print(f"question_text: {question_text}")
         # print(f"top1: {bm25.get_top_n(tokenized_query, self.knowledge_candidate, n=1)}")
@@ -95,9 +103,17 @@ class DPR:
         self.knowledge_candidate = knowledge_text
 
     def similarities(self, question_text):
-        knowledge_candidate_embeddings = self.passage_encoder.encode(self.knowledge_candidate, show_progress_bar=False)
-        question_embedding = self.query_encoder.encode(question_text, show_progress_bar=False)
-        scores = util.dot_score(knowledge_candidate_embeddings, question_embedding).squeeze().tolist()
+        knowledge_candidate_embeddings = self.passage_encoder.encode(
+            self.knowledge_candidate, show_progress_bar=False
+        )
+        question_embedding = self.query_encoder.encode(
+            question_text, show_progress_bar=False
+        )
+        scores = (
+            util.dot_score(knowledge_candidate_embeddings, question_embedding)
+            .squeeze()
+            .tolist()
+        )
         for idx, qt in enumerate(scores):
             self.idx_and_similarity_list.append([idx, qt])
 
@@ -107,10 +123,12 @@ class DPR:
         passages = [
             "London [SEP] London is the capital and largest city of England and the United Kingdom.",
             "Paris [SEP] Paris is the capital and most populous city of France.",
-            "Berlin [SEP] Berlin is the capital and largest city of Germany by both area and population."
+            "Berlin [SEP] Berlin is the capital and largest city of Germany by both area and population.",
         ]
 
-        passage_embeddings = self.passage_encoder.encode(passages, show_progress_bar=False)
+        passage_embeddings = self.passage_encoder.encode(
+            passages, show_progress_bar=False
+        )
         query = "What is the capital of England?"
         query_embedding = self.query_encoder.encode(query, show_progress_bar=False)
 
@@ -132,53 +150,67 @@ class STS:
         self.knowledge_candidate = knowledge_text
 
     def similarities(self, question_text):
-        knowledge_candidate_embeddings = self.model.encode(self.knowledge_candidate, show_progress_bar=False)
+        knowledge_candidate_embeddings = self.model.encode(
+            self.knowledge_candidate, show_progress_bar=False
+        )
         question_embedding = self.model.encode(question_text, show_progress_bar=False)
-        scores = util.dot_score(knowledge_candidate_embeddings, question_embedding).squeeze().tolist()
+        scores = (
+            util.dot_score(knowledge_candidate_embeddings, question_embedding)
+            .squeeze()
+            .tolist()
+        )
         for idx, qt in enumerate(scores):
             self.idx_and_similarity_list.append([idx, qt])
 
         return self.idx_and_similarity_list
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sim_method = TfIdf()
     passages = [
         "Paris is the capital and most populous city of France.",
         "Berlin is the capital and largest city of Germany by both area and population.",
-        "London is the capital and largest city of England and the United Kingdom."
+        "London is the capital and largest city of England and the United Kingdom.",
     ]
     query = "What is the capital of England?"
     for i, paragraph in enumerate(passages):
         sim_method.add_document(i, paragraph)
-    print(f"TFIDF: {sorted(sim_method.similarities(query), key=lambda x: x[1], reverse=True)}")
+    print(
+        f"TFIDF: {sorted(sim_method.similarities(query), key=lambda x: x[1], reverse=True)}"
+    )
 
     sim_method = BM25()
     passages = [
         "Paris is the capital and most populous city of France.",
         "Berlin is the capital and largest city of Germany by both area and population.",
-        "London is the capital and largest city of England and the United Kingdom."
+        "London is the capital and largest city of England and the United Kingdom.",
     ]
     query = "What is the capital of England?"
     sim_method.add_document(passages)
-    print(f"BM25: {sorted(sim_method.similarities(query), key=lambda x:x[1], reverse=True)}")
+    print(
+        f"BM25: {sorted(sim_method.similarities(query), key=lambda x:x[1], reverse=True)}"
+    )
 
     sim_method = DPR()
     passages = [
         "Paris is the capital and most populous city of France.",
         "Berlin is the capital and largest city of Germany by both area and population.",
-        "London is the capital and largest city of England and the United Kingdom."
+        "London is the capital and largest city of England and the United Kingdom.",
     ]
     query = "What is the capital of England?"
     sim_method.add_document(passages)
-    print(f"DPR: {sorted(sim_method.similarities(query), key=lambda x:x[1], reverse=True)}")
+    print(
+        f"DPR: {sorted(sim_method.similarities(query), key=lambda x:x[1], reverse=True)}"
+    )
 
     sim_method = STS()
     passages = [
         "Paris is the capital and most populous city of France.",
         "Berlin is the capital and largest city of Germany by both area and population.",
-        "London is the capital and largest city of England and the United Kingdom."
+        "London is the capital and largest city of England and the United Kingdom.",
     ]
     query = "What is the capital of England?"
     sim_method.add_document(passages)
-    print(f"STS: {sorted(sim_method.similarities(query), key=lambda x:x[1], reverse=True)}")
+    print(
+        f"STS: {sorted(sim_method.similarities(query), key=lambda x:x[1], reverse=True)}"
+    )
